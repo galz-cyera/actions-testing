8950855f41f01f900a177a2a596c95ffdbe09d80
gal zollman <galz@cyera.io>
2022-04-17 10:43:44 +0300

    test

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyChar111m111111111')     #alksdjalksdasd
+    print_hi('PyChar111m1111111111')     #alksdjalksdasd
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 # asdasda
