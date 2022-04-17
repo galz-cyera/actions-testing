ffc96ff4ea9960f6f29710ab24007a13e8bb9b58
gal zollman <galz@cyera.io>
2022-04-17 10:03:21 +0300

    test

--- main2.py
+++ main2.py
@@ -11,7 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('222223')
+    print_hi('2222231')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 # asdasda
